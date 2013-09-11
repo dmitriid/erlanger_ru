09e8485061d664586d11a947a8533e9e6e60b381
Dmytro Lytovchenko <kvakvs@yandex.ru>
2013-09-11 22:42:57 +0200

    twitter feed

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,6 @@ class WelcomeController < ApplicationController
   def index
     @frontpage_news = News.all
     @frontpage_events = Event.all
+    @tweets = Twitter.search("#erlang -rt", :lang => "ru", :count => 5).results
   end
 end
