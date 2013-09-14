3f2bb97b8ddefcff624836a64b080a3e2c109523
Dmytro Lytovchenko <kvakvs@yandex.ru>
2013-09-14 23:38:58 +0200

    rome2rio

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,8 +4,16 @@ class EventsController < ApplicationController
   end
 
   def show
-    @event = Event.includes(:i18n_events).
-        where("i18n_events.lang_iso639" => I18n.locale.to_s).take
+    # TODO: If locale is not RU and object is not found, query RU locale
+    # @event = get_event(lang) || get_event(SOMETHING.config.i18n.default_locale.SOMETHING)
+    @event = EventsController::get_event(I18n.locale.to_s) ||
+        EventsController::get_event("ru")
+  end
+
+  # static
+  def self.get_event(lang)
+    Event.includes(:i18n_events).
+        where("i18n_events.lang_iso639" => lang).take
   end
 
   # static
