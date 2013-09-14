01fe7b1a66b65df2a3e4f1aa09dfa57dd0a5176f
Dmytro Lytovchenko <kvakvs@yandex.ru>
2013-09-14 21:09:23 +0200

    country seeds + site footer

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,9 +19,12 @@ e.i18n_events.create(event_id: e.id, lang_iso639: "en",
                      title: "EUC 2014", city: "Stockholm",
                      info: "Derp")
 
-Country.create(country_iso3166: "se", lang_iso639: "en",
-               country_name: "Sweden", lang_name: "swedish")
-Country.create(country_iso3166: "se", lang_iso639: "ru",
-               country_name: "Швеция", lang_name: "шведский")
-Country.create(country_iso3166: "se", lang_iso639: "ua",
-               country_name: "Швеція", lang_name: "шведська")
+Country.create(country_iso3166: "se", lang_iso639: "en", country_name: "Sweden", lang_name: "swedish")
+Country.create(country_iso3166: "se", lang_iso639: "ru", country_name: "Швеция", lang_name: "шведский")
+Country.create(country_iso3166: "se", lang_iso639: "ua", country_name: "Швеція", lang_name: "шведська")
+Country.create(country_iso3166: "ru", lang_iso639: "en", country_name: "Russia", lang_name: "russian")
+Country.create(country_iso3166: "ru", lang_iso639: "ru", country_name: "Россия", lang_name: "русский")
+Country.create(country_iso3166: "ru", lang_iso639: "ua", country_name: "Росія", lang_name: "російська")
+Country.create(country_iso3166: "ua", lang_iso639: "en", country_name: "Ukraine", lang_name: "ukrainian")
+Country.create(country_iso3166: "ua", lang_iso639: "ru", country_name: "Украина", lang_name: "украинский")
+Country.create(country_iso3166: "ua", lang_iso639: "ua", country_name: "Україна", lang_name: "українська")
