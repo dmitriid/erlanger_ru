23596a036422a84650922660db86789d645491ae
Dmytro Lytovchenko <kvakvs@yandex.ru>
2013-09-15 00:48:20 +0200

    article tags

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,8 @@
 class Article < ActiveRecord::Base
   has_many :i18n_articles
-  has_many :article_tags
-  has_many :tags, :through => :article_tags
+
+  def get_tags
+    lang = I18n.locale.to_s
+    ArticleTag.includes(:tag).where("tags.lang_iso639" => lang).to_a
+  end
 end
--- app/models/news_tag.rb
+++ app/models/news_tag.rb
@@ -1,4 +1,3 @@
 class NewsTag < ActiveRecord::Base
   belongs_to :tag
-  #has_and_belongs_to_many :tag
 end
