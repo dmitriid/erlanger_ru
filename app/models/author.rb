94f405010af781ff46b3996f74c1e09ba8434d9f
Dmitrii Dimandt <dmitrii@dmitriid.com>
2013-09-23 19:30:01 +0200

    authors & external links

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,15 @@
 class Author < ActiveRecord::Base
   belongs_to :resource
   include ResourceHelper
+
+  def initials
+    '' if self.name.nil?
+
+    s = self.name.split
+    one = s[0][0].upcase
+    two = ''
+    two = s[1][0].upcase unless s[1].nil?
+
+    "#{one}#{two}"
+  end
 end
